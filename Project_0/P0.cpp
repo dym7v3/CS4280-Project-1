c3ff84c53dbee4ef4097b86630aaada8b5255cb5
Dennis Moyseyev <denomoyseyev@gmail.com>
2017-09-20 17:50:27 -0500

    Uploading the first time.

---
+++ Project_0/P0.cpp
@@ -0,0 +1,16 @@
+/*
+ * P0.cpp
+ *
+ *  Created on: Sep 20, 2017
+ *      Author: Dennis Moyseyev
+ */
+
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
+
