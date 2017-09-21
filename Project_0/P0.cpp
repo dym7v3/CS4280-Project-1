4211998c44dbbc8d1c2124f6b9b14c672b1684e0
Dennis Moyseyev <denomoyseyev@gmail.com>
2017-09-20 22:59:20 -0500

    User input is almost done. Need to figure out how stdin works.

--- Project_0/P0.cpp
+++ Project_0/P0.cpp
@@ -5,12 +5,39 @@
  *      Author: Dennis Moyseyev
  */
 
-
+#include <fstream>
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main ( int argc, char *argv[] )
 {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
+
+	FILE *fp; //Creates a file pointer.
+
+	cout<< argv[0];
+
+	if(argc==1)
+	{
+		fp=stdin;
+	}
+
+	else
+	{
+		ifstream the_file ( argv[1] );
+
+		//Checks if the file was actually opened.
+		if ( !the_file.is_open() )
+			cout<<"Could not open file\n"; //Prints out an error message
+
+	    else
+	    {
+	    	  char x;
+	      	  while ( the_file.get ( x ) )
+	    	  cout<< x;
+	    }
+	}
+
+
+  }
 
